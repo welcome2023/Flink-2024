099c5c9a6f448e4d95cccc6a7996ed91ce3d004d
cms <cmsxyz@163.com>
2023-12-29 23:54:27 +0800

    ./pom.xml

---
+++ src/main/java/org/example/streaming/DivideStream.java
@@ -0,0 +1,14 @@
+package org.example.streaming;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @createDate 2023-12-29 20:22
+ */
+
+@Slf4j
+public class DivideStream {
+    public static void main(String[] args) {
+
+    }
+}
